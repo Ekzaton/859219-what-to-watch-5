475ae49c317d0835d9aeaf3b424ab2848e47bc9d
Ekzaton <ekzaton@gmail.com>
2020-10-24 17:26:36 +0500

    Created 'review-form.jsx' component

---
+++ src/components/review-form/review-form.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+class ReviewForm extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ratingValue: ``,
+      textValue: ``,
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onRatingChange = this.onRatingChange.bind(this);
+    this.onTextChange = this.onTextChange.bind(this);
+  }
+
+  onSubmit(evt) {
+    evt.preventDefault();
+
+    this.setState({ratingValue: ``, textValue: ``});
+  }
+
+  onRatingChange(evt) {
+    this.setState({ratingValue: evt.target.value});
+  }
+
+  onTextChange(evt) {
+    this.setState({textValue: evt.target.value});
+  }
+
+  render() {
+    return (
+      <form action="#" className="add-review__form" onSubmit={this.onSubmit}>
+        <div className="rating">
+          <div className="rating__stars">
+            <input
+              className="rating__input"
+              id="star-1"
+              type="radio"
+              name="rating"
+              value="1"
+              checked={this.state.ratingValue === `1`}
+              onChange={this.onRatingChange}
+            />
+            <label className="rating__label" htmlFor="star-1">Rating 1</label>
+
+            <input
+              className="rating__input"
+              id="star-2"
+              type="radio"
+              name="rating"
+              value="2"
+              checked={this.state.ratingValue === `2`}
+              onChange={this.onRatingChange}
+            />
+            <label className="rating__label" htmlFor="star-2">Rating 2</label>
+
+            <input
+              className="rating__input"
+              id="star-3"
+              type="radio"
+              name="rating"
+              value="3"
+              checked={this.state.ratingValue === `3`}
+              onChange={this.onRatingChange}
+            />
+            <label className="rating__label" htmlFor="star-3">Rating 3</label>
+
+            <input
+              className="rating__input"
+              id="star-4"
+              type="radio"
+              name="rating"
+              value="4"
+              checked={this.state.ratingValue === `4`}
+              onChange={this.onRatingChange}
+            />
+            <label className="rating__label" htmlFor="star-4">Rating 4</label>
+
+            <input
+              className="rating__input"
+              id="star-5"
+              type="radio"
+              name="rating"
+              value="5"
+              checked={this.state.ratingValue === `5`}
+              onChange={this.onRatingChange}
+            />
+            <label className="rating__label" htmlFor="star-5">Rating 5</label>
+          </div>
+        </div>
+
+        <div className="add-review__text">
+          <textarea
+            className="add-review__textarea"
+            name="review-text"
+            id="review-text"
+            placeholder="Review text"
+            value={this.state.textValue}
+            onChange={this.onTextChange}
+          ></textarea>
+          <div className="add-review__submit">
+            <button className="add-review__btn" type="submit">Post</button>
+          </div>
+
+        </div>
+      </form>
+    );
+  }
+}
+
+export default ReviewForm;
--- src/components/review/review.jsx
+++ src/components/review/review.jsx
@@ -3,92 +3,61 @@ import {Link} from "react-router-dom";
 
 import {moviesValidator} from "../../validators";
 
-class Review extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
+import ReviewForm from "../review-form/review-form";
 
-  render() {
-    const {movie} = this.props;
+const Review = (props) => {
+  const {movie} = props;
 
-    return (
-      <section className="movie-card movie-card--full">
-        <div className="movie-card__header">
-          <div className="movie-card__bg">
-            <img src={movie.cardImage} alt={movie.title}/>
-          </div>
-
-          <h1 className="visually-hidden">WTW</h1>
-
-          <header className="page-header">
-            <div className="logo">
-              <Link to="/" className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </Link>
-            </div>
-
-            <nav className="breadcrumbs">
-              <ul className="breadcrumbs__list">
-                <li className="breadcrumbs__item">
-                  <Link to="/movies/:id" className="breadcrumbs__link">
-                    {movie.title}
-                  </Link>
-                </li>
-                <li className="breadcrumbs__item">
-                  <a className="breadcrumbs__link">Add review</a>
-                </li>
-              </ul>
-            </nav>
-
-            <div className="user-block">
-              <div className="user-block__avatar">
-                <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-              </div>
-            </div>
-          </header>
-
-          <div className="movie-card__poster movie-card__poster--small">
-            <img src={movie.posterImage} alt={`${movie.title} poster`} width="218" height="327"/>
-          </div>
+  return (
+    <section className="movie-card movie-card--full">
+      <div className="movie-card__header">
+        <div className="movie-card__bg">
+          <img src={movie.cardImage} alt={movie.title}/>
         </div>
 
-        <div className="add-review">
-          <form action="#" className="add-review__form">
-            <div className="rating">
-              <div className="rating__stars">
-                <input className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
-                <label className="rating__label" htmlFor="star-1">Rating 1</label>
+        <h1 className="visually-hidden">WTW</h1>
 
-                <input className="rating__input" id="star-2" type="radio" name="rating" value="2"/>
-                <label className="rating__label" htmlFor="star-2">Rating 2</label>
+        <header className="page-header">
+          <div className="logo">
+            <Link to="/" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </Link>
+          </div>
 
-                <input className="rating__input" id="star-3" type="radio" name="rating" value="3" checked/>
-                <label className="rating__label" htmlFor="star-3">Rating 3</label>
+          <nav className="breadcrumbs">
+            <ul className="breadcrumbs__list">
+              <li className="breadcrumbs__item">
+                <Link to="/movies/:id" className="breadcrumbs__link">
+                  {movie.title}
+                </Link>
+              </li>
+              <li className="breadcrumbs__item">
+                <a className="breadcrumbs__link">Add review</a>
+              </li>
+            </ul>
+          </nav>
 
-                <input className="rating__input" id="star-4" type="radio" name="rating" value="4"/>
-                <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-                <input className="rating__input" id="star-5" type="radio" name="rating" value="5"/>
-                <label className="rating__label" htmlFor="star-5">Rating 5</label>
-              </div>
+          <div className="user-block">
+            <div className="user-block__avatar">
+              <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63"/>
             </div>
+          </div>
+        </header>
 
-            <div className="add-review__text">
-              <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
-              <div className="add-review__submit">
-                <button className="add-review__btn" type="submit">Post</button>
-              </div>
-
-            </div>
-          </form>
+        <div className="movie-card__poster movie-card__poster--small">
+          <img src={movie.posterImage} alt={`${movie.title} poster`} width="218" height="327"/>
         </div>
+      </div>
 
-      </section>
-    );
-  }
-}
+      <div className="add-review">
+        <ReviewForm/>
+      </div>
+
+    </section>
+  );
+};
 
 Review.propTypes = {
   movie: moviesValidator,
