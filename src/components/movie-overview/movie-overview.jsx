af9254f07ef0433c2057ef14307e2ff9e41778e8
Ekzaton <ekzaton@gmail.com>
2020-11-03 12:29:41 +0500

    Transferred props to `movie-overview.jsx` component

--- src/components/movie-overview/movie-overview.jsx
+++ src/components/movie-overview/movie-overview.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 
-const MovieOverview = () => {
+import {movieType} from "../../types";
+
+const MovieOverview = (props) => {
+  const {movie} = props;
+
   return (
     <React.Fragment>
       <div className="movie-rating">
-        <div className="movie-rating__score">8,9</div>
+        <div className="movie-rating__score">{movie.ratingScore}</div>
         <p className="movie-rating__meta">
-          <span className="movie-rating__level">Very good</span>
-          <span className="movie-rating__count">240 ratings</span>
+          <span className="movie-rating__level">{movie.ratingLevel}</span>
+          <span className="movie-rating__count">{movie.ratingCount}</span>
         </p>
       </div>
 
       <div className="movie-card__text">
-        <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes).</p>
-        <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-        <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
+        <p>{movie.text}</p>
+        <p className="movie-card__director"><strong>Director: {movie.director}</strong></p>
+        <p className="movie-card__starring"><strong>Starring: {movie.starring}</strong></p>
       </div>
     </React.Fragment>
   );
+};
+
+MovieOverview.propTypes = {
+  movie: movieType,
 };
 
 export default MovieOverview;
