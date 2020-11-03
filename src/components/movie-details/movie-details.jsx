b0db75436d2a6045f2cf054852edaf765cd9a15d
Ekzaton <ekzaton@gmail.com>
2020-11-03 12:35:15 +0500

    Transferred props to `movie-details.jsx` component

--- src/components/movie-details/movie-details.jsx
+++ src/components/movie-details/movie-details.jsx
@@ -1,48 +1,43 @@
 import React from "react";
 
-const MovieDetails = () => {
+import {movieType} from "../../types";
+
+const MovieDetails = (props) => {
+  const {movie} = props;
+
   return (
     <div className="movie-card__text movie-card__row">
       <div className="movie-card__text-col">
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Director</strong>
-          <span className="movie-card__details-value">Wes Andreson</span>
+          <span className="movie-card__details-value">{movie.director}</span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Starring</strong>
-          <span className="movie-card__details-value">
-            Bill Murray, <br/>
-            Edward Norton, <br/>
-            Jude Law, <br/>
-            Willem Dafoe, <br/>
-            Saoirse Ronan, <br/>
-            Tony Revoloru, <br/>
-            Tilda Swinton, <br/>
-            Tom Wilkinson, <br/>
-            Owen Wilkinson, <br/>
-            Adrien Brody, <br/>
-            Ralph Fiennes, <br/>
-            Jeff Goldblum <br/>
-          </span>
+          <span className="movie-card__details-value">{movie.starring}</span>
         </p>
       </div>
 
       <div className="movie-card__text-col">
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Run Time</strong>
-          <span className="movie-card__details-value">1h 39m</span>
+          <span className="movie-card__details-value">{movie.runTime}</span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Genre</strong>
-          <span className="movie-card__details-value">Comedy</span>
+          <span className="movie-card__details-value">{movie.genre}</span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Released</strong>
-          <span className="movie-card__details-value">2014</span>
+          <span className="movie-card__details-value">{movie.year}</span>
         </p>
       </div>
     </div>
   );
+};
+
+MovieDetails.propTypes = {
+  movie: movieType,
 };
 
 export default MovieDetails;
