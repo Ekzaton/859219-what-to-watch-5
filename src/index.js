02a07aaaf2ad1d409d0bdef93d0306240aea2b15
Ekzaton <ekzaton@gmail.com>
2020-11-08 18:17:58 +0500

    Connected redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+import {reducer} from "./store/reducer";
+
 import App from "./components/app/app";
 
 import movies from "./mocks/movies";
 import reviews from "./mocks/reviews";
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App movies={movies} reviews={reviews}/>,
+    <Provider store={store}>
+      <App movies={movies} reviews={reviews}/>
+    </Provider>,
     document.querySelector(`#root`)
 );
