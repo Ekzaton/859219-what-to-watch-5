80edf385c5f461d11d965c2e10f50be5c00ce55c
Ekzaton <ekzaton@gmail.com>
2020-11-08 18:18:49 +0500

    Added Redux DevTools support

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ import App from "./components/app/app";
 import movies from "./mocks/movies";
 import reviews from "./mocks/reviews";
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
