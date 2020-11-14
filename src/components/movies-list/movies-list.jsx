3cb21da6861b07cd0dd706f64050ff0c4a535802
Ekzaton <ekzaton@gmail.com>
2020-11-14 19:59:57 +0500

    Refactored 'movies-list.jsx' component

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -5,48 +5,30 @@ import {movieType} from "../../types";
 
 import MoviesItem from "../movies-item/movies-item";
 
-class MoviesList extends React.PureComponent {
-  constructor(props) {
-    super(props);
+const MoviesList = (props) => {
+  const {movies, shownMovies, onMovieEnter, onMovieLeave, activeMovieId} = props;
 
-    this.state = {
-      activeMovieId: -1,
-    };
-
-    this.handleMovieActivation = this.handleMovieActivation.bind(this);
-    this.handleMovieDeactivation = this.handleMovieDeactivation.bind(this);
-  }
-
-  handleMovieActivation(id) {
-    this.setState({activeMovieId: id});
-  }
-
-  handleMovieDeactivation() {
-    this.setState({activeMovieId: -1});
-  }
-
-  render() {
-    const {movies, shownMovies} = this.props;
-
-    return (
-      <div className="catalog__movies-list">
-        {movies.map((movie, i) => i < shownMovies &&
-          <MoviesItem
-            key={`movie-${i}`}
-            movie={movie}
-            onMovieEnter={this.handleMovieActivation}
-            onMovieLeave={this.handleMovieDeactivation}
-            isPlaying={this.state.activeMovieId === movie.id}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="catalog__movies-list">
+      {movies.map((movie, i) => i < shownMovies &&
+        <MoviesItem
+          key={`movie-${i}`}
+          movie={movie}
+          onMovieEnter={onMovieEnter}
+          onMovieLeave={onMovieLeave}
+          isPlaying={activeMovieId === movie.id}
+        />
+      )}
+    </div>
+  );
+};
 
 MoviesList.propTypes = {
   movies: PropTypes.arrayOf(movieType),
   shownMovies: PropTypes.number.isRequired,
+  onMovieEnter: PropTypes.func.isRequired,
+  onMovieLeave: PropTypes.func.isRequired,
+  activeMovieId: PropTypes.number.isRequired,
 };
 
 export default MoviesList;
