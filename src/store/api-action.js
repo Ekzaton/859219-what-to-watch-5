1019cae8d9d9f7977cea5b5791f880b762747f33
Ekzaton <ekzaton@gmail.com>
2020-11-20 09:12:16 +0500

    Added api action

---
+++ src/store/api-action.js
@@ -0,0 +1,6 @@
+import {ActionCreator} from "./action";
+
+export const fetchMoviesList = () => (dispatch, _getState, api) => (
+  api.get(`films`)
+    .then(({data}) => dispatch(ActionCreator.getMovies(data)))
+);
