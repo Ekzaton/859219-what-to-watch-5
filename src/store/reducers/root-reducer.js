d3ce2cb496aa4fe89b785c750d17a20d9e3792ef
Ekzaton <ekzaton@gmail.com>
2020-11-22 02:01:31 +0500

    Connected reducer

--- src/store/reducers/root-reducer.js
+++ src/store/reducers/root-reducer.js
@@ -2,13 +2,16 @@ import {combineReducers} from "redux";
 
 import {appData} from "./app-data/app-data";
 import {appState} from "./app-state/app-state";
+import {appUser} from "./app-user/app-user";
 
 export const NameSpace = {
   APP_DATA: `APP_DATA`,
   APP_STATE: `APP_STATE`,
+  APP_USER: `APP_USER`,
 };
 
 export default combineReducers({
   [NameSpace.APP_DATA]: appData,
   [NameSpace.APP_STATE]: appState,
+  [NameSpace.APP_USER]: appUser,
 });
