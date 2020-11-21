dabf48e65c07c1503a79be2561a126d1b1af73e4
Ekzaton <ekzaton@gmail.com>
2020-11-22 01:58:46 +0500

    Added api action for authorization

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,6 +1,13 @@
 import {movieAdapter, reviewAdapter} from "../services/adapters";
 
-import {getAllMovies, getPromoMovie, getMovieReviews} from "./actions";
+import {
+  getAllMovies,
+  getPromoMovie,
+  getMovieReviews,
+  requiredAuthorization
+} from "./actions";
+
+import {AuthorizationStatus} from "../const";
 
 export const fetchAllMovies = () => (dispatch, _getState, api) => (
   api.get(`/films`)
@@ -15,4 +22,12 @@ export const fetchPromoMovie = () => (dispatch, _getState, api) => (
 export const fetchMovieReviews = (id) => (dispatch, _getState, api) => (
   api.get(`/comments/${id}`)
     .then(({data}) => dispatch(getMovieReviews(data.map(reviewAdapter))))
+);
+
+export const checkAuthorization = () => (dispatch, _getState, api) => (
+  api.get(`/login`)
+    .then(() => dispatch(requiredAuthorization(AuthorizationStatus.AUTH)))
+    .catch((err) => {
+      throw err;
+    })
 );
