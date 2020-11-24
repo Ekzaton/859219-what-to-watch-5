ebfd5edbf2667f714b003e0346a4b9d54fbe22ef
Ekzaton <ekzaton@gmail.com>
2020-11-24 18:08:56 +0500

    Created 'user.jsx' component

---
+++ src/components/user/user.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+
+import {AuthorizationStatus} from "../../const";
+
+const User = (props) => {
+  const {status} = props;
+  const isAuthorized = (status === AuthorizationStatus.AUTH);
+
+  return (
+    <React.Fragment>
+      {isAuthorized
+        ? <div className="user-block__avatar">
+          <Link to="/my-list">
+            <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+          </Link>
+        </div>
+        : <div className="user-block__link">
+          <Link to="/sign-in" className="user-block__link">Sign in</Link>
+        </div>
+      }
+    </React.Fragment>
+  );
+};
+
+User.propTypes = {
+  status: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({APP_USER}) => ({
+  status: APP_USER.status,
+});
+
+export {User};
+export default connect(mapStateToProps, null)(User);
