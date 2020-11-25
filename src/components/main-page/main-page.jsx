98a5666a7b12741013b17594a5f1fbbf54f0a04c
Ekzaton <ekzaton@gmail.com>
2020-11-25 21:38:26 +0500

    Connected 'promo-movie.jsx' component

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,77 +1,12 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {connect} from "react-redux";
-
-import {AppRoute} from "../../const";
-import {movieType} from "../../types";
 
 import MoviesCatalog from "../movies-catalog/movies-catalog";
-import MyListButton from "../my-list-button/my-list-button";
-import User from "../user/user";
+import PromoMovie from "../promo-movie/promo-movie";
 
-const MainPage = (props) => {
-  const {promoMovie} = props;
-
+const MainPage = () => {
   return (
     <React.Fragment>
-      <section className="movie-card">
-        <div className="movie-card__bg">
-          <img src={promoMovie.bgImage} alt={promoMovie.title}/>
-        </div>
-
-        <h1 className="visually-hidden">WTW</h1>
-
-        <header className="page-header movie-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="user-block">
-            <User/>
-          </div>
-        </header>
-
-        <div className="movie-card__wrap">
-          <div className="movie-card__info">
-            <div className="movie-card__poster">
-              <img
-                src={promoMovie.posterImage}
-                alt={`${promoMovie.title} poster`}
-                width="218"
-                height="327"
-              />
-            </div>
-
-            <div className="movie-card__desc">
-              <h2 className="movie-card__title">{promoMovie.title}</h2>
-              <p className="movie-card__meta">
-                <span className="movie-card__genre">{promoMovie.genre}</span>
-                <span className="movie-card__year">{promoMovie.year}</span>
-              </p>
-
-              <div className="movie-card__buttons">
-                <Link
-                  to={`${AppRoute.PLAYER}${promoMovie.id}`}
-                  className="btn btn--play movie-card__button"
-                >
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </Link>
-                <MyListButton
-                  id={promoMovie.id}
-                  isFavorite={promoMovie.isFavorite}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <PromoMovie/>
 
       <div className="page-content">
         <MoviesCatalog/>
@@ -94,15 +29,4 @@ const MainPage = (props) => {
   );
 };
 
-const mapStateToProps = ({APP_DATA}) => {
-  return {
-    promoMovie: APP_DATA.promoMovie,
-  };
-};
-
-MainPage.propTypes = {
-  promoMovie: movieType,
-};
-
-export {MainPage};
-export default connect(mapStateToProps, null)(MainPage);
+export default MainPage;
