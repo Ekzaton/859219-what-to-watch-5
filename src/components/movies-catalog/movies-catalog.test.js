8b8aedd0889ae61164bff8bad354ebfee8e3ca09
Ekzaton <ekzaton@gmail.com>
2020-11-26 17:20:17 +0500

    Added snapshot test for 'movies-catalog.jsx' component

---
+++ src/components/movies-catalog/movies-catalog.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+import {Route, BrowserRouter} from "react-router-dom";
+
+import mockMovies from "../../mocks/movies";
+
+import MoviesCatalog from "./movies-catalog";
+
+describe(`MoviesCatalog snapshot testing`, () => {
+  const mockStore = configureStore([]);
+
+  const store = mockStore({
+    APP_DATA: {
+      movies: mockMovies,
+    },
+    APP_STATE: {
+      activeGenre: `Action`,
+      moviesByGenre: mockMovies,
+      shownMovies: 0,
+    }
+  });
+
+  const moviesCatalogComponent = renderer.create(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Route>
+            <MoviesCatalog/>
+          </Route>
+        </BrowserRouter>
+      </Provider>
+  );
+
+  it(`renders component correctly`, () => {
+    expect(moviesCatalogComponent.toJSON()).toMatchSnapshot();
+  });
+});
