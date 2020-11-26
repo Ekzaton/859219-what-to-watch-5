890cbd656ec43d304927187c99e6e1cb4dde242f
Ekzaton <ekzaton@gmail.com>
2020-11-26 14:49:50 +0500

    Added snapshot test for 'main-page.jsx' component

---
+++ src/components/main-page/main-page.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+import {Route, BrowserRouter} from "react-router-dom";
+
+import mockMovies from "../../mocks/movies";
+import mockPromo from "../../mocks/promo";
+
+import MainPage from "./main-page";
+
+describe(`MainPage component`, () => {
+  const mockStore = configureStore([]);
+
+  const store = mockStore({
+    APP_DATA: {
+      movies: mockMovies,
+      promoMovie: mockPromo,
+    },
+    APP_STATE: {
+      activeGenre: `Crime`,
+      moviesByGenre: mockMovies,
+      shownMovies: 0,
+    },
+    APP_USER: {
+      status: `NO_AUTH`,
+    }
+  });
+
+  const mainPageComponent = renderer.create(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Route>
+            <MainPage/>
+          </Route>
+        </BrowserRouter>
+      </Provider>
+  );
+
+  it(`Renders store-connected component correctly`, () => {
+    expect(mainPageComponent.toJSON()).toMatchSnapshot();
+  });
+});
