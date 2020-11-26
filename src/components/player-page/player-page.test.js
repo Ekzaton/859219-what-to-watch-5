c61d12ad2741acb0910b80f1a4837aed03c8c406
Ekzaton <ekzaton@gmail.com>
2020-11-26 22:43:41 +0500

    Added snapshot test for 'player-page.jsx' component

---
+++ src/components/player-page/player-page.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+import {Route, BrowserRouter} from "react-router-dom";
+
+import mockPromo from "../../mocks/promo";
+
+import PlayerPage from "./player-page";
+
+describe(`PlayerPage snapshot testing`, () => {
+  it(`renders store-connected component correctly`, () => {
+    const ref = React.createRef();
+    const mockStore = configureStore([]);
+
+    const store = mockStore({
+      APP_DATA: {
+        activeMovie: mockPromo,
+      }
+    });
+
+    const playerPageComponent = renderer.create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <Route>
+              <PlayerPage
+                progressRef={ref}
+                videoRef={ref}
+                isPlaying={true}
+                progressBar={33}
+                timeRemaining={1200}
+                handleFullScreenButton={() => {}}
+                handlePlayButton={() => {}}
+                handleMouseDown={() => {}}
+              />
+            </Route>
+          </BrowserRouter>
+        </Provider>
+    );
+
+    expect(playerPageComponent.toJSON()).toMatchSnapshot();
+  });
+});
