4652a8f793520fc6dc177434b0df3792fc79cf5c
Ekzaton <ekzaton@gmail.com>
2020-11-26 13:55:58 +0500

    Added new snapshot test for 'genres-list.jsx' component

---
+++ src/components/genres-list/genres-list.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+
+import mockMovies from "../../mocks/movies";
+
+import GenresList from "./genres-list";
+
+describe(`GenresList component`, () => {
+  const mockStore = configureStore([]);
+  let store = null;
+  let genresListComponent = null;
+
+  beforeEach(() => {
+    store = mockStore({
+      APP_DATA: {
+        movies: mockMovies,
+      },
+      APP_STATE: {
+        activeGenre: `Action`,
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    genresListComponent = renderer.create(
+        <Provider store={store}>
+          <GenresList
+            onGenreClick={() => {}}
+          />
+        </Provider>
+    );
+  });
+
+  it(`Renders store-connected component correctly`, () => {
+    expect(genresListComponent.toJSON()).toMatchSnapshot();
+  });
+});
