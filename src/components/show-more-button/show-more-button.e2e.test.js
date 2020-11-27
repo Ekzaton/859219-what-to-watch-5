291bc1ee8733068a75c88667dffc165eb8740f79
Ekzaton <ekzaton@gmail.com>
2020-11-27 15:17:33 +0500

    Added e2e test for 'show-more-button.jsx' component

---
+++ src/components/show-more-button/show-more-button.e2e.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import {ShowMoreButton} from "./show-more-button";
+
+describe(`ShowMoreButton e2e testing`, () => {
+  configure({adapter: new Adapter()});
+
+  it(`execute callback on button click`, () => {
+    const onShowMoreClick = jest.fn();
+
+    const showMoreButtonComponent = shallow(
+        <ShowMoreButton
+          shownMovies={8}
+          onShowMoreClick={onShowMoreClick}
+        />
+    );
+
+    const catalogButton = showMoreButtonComponent.find(`.catalog__button`);
+    catalogButton.simulate(`click`);
+    expect(onShowMoreClick).toHaveBeenCalledTimes(1);
+  });
+});
