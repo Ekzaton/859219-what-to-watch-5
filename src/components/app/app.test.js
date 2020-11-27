4f22186be29f5b4bdfe83577bb7dfe61dfb4bf8f
Ekzaton <ekzaton@gmail.com>
2020-11-27 07:17:56 +0500

    Added snapshot test for 'app.jsx' component

---
+++ src/components/app/app.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+
+import {AuthorizationStatus, ALL_GENRES} from "../../const";
+
+import mockMovies from "../../mocks/movies";
+import mockPromo from "../../mocks/promo";
+
+import App from "./app";
+
+describe(`App snapshot testing`, () => {
+  const mockStore = configureStore([]);
+
+  const store = mockStore({
+    APP_DATA: {
+      movies: mockMovies,
+      promoMovie: mockPromo,
+    },
+    APP_STATE: {
+      activeGenre: ALL_GENRES,
+      promoMovie: mockPromo,
+    },
+    APP_USER: {
+      status: AuthorizationStatus.NO_AUTH,
+    }
+  });
+
+  const appComponent = renderer.create(
+      <Provider store={store}>
+        <App/>
+      </Provider>
+  );
+
+  it(`renders component correctly`, () => {
+    expect(appComponent.toJSON()).toMatchSnapshot();
+  });
+});
